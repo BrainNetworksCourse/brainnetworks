cbd19e316b21e19e0de916ed3c88e90195334194
poldrack <poldrack@gmail.com>
2018-10-01 09:24:54 -0700

    updating to version 0.2.1

--- brainnetworks/data/facebook/README.md
+++ brainnetworks/data/facebook/README.md
@@ -1,1 +1,1 @@
-Facebook friendship data obtained from the Stanford Large Network Database: https://snap.stanford.edu/data/egonets-Facebook.html
+Facebook friendship data redistributed by permission from the Stanford Large Network Database: https://snap.stanford.edu/data/egonets-Facebook.html
--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
-from distutils.core import setup
-from setuptools import find_packages
+from setuptools import find_packages,setup
 
 packages=find_packages()
 print('found packages:',packages)
 setup(
     name='brainnetworks',
-    version='0.1',
+    version='0.2',
     packages=packages,
+    include_package_data=True,
     license='MIT License',
     long_description=open('README.md').read(),
 )
